d07476cdb111a47b09107110abea9044c50acb9a
Ernesto Mello <emello969@gmail.com>
2022-08-12 14:35:00 -0300

    Se deja los modelos Persona, Socio y categoría Socio. Se configura el Admin para que se puedan dar el alta de persona, socios y categorías de socios. Queda hecho el migrate de todos los modelos.

--- gestionAbu/socios/admin.py
+++ gestionAbu/socios/admin.py
@@ -1,3 +1,16 @@
+from atexit import register
 from django.contrib import admin
+from socios.models import Persona, Socio, Categoria_socio
 
 # Register your models here.
+class PersonaAdmin(admin.ModelAdmin):
+    list_display = ('id','nombre','apellido_paterno','correo_electronico','celular')
+admin.site.register(Persona,PersonaAdmin)
+
+class SocioAdmin(admin.ModelAdmin):
+    list_display = ('id_persona','categoria_socio','frecuencia_pago','fecha_ingreso','fecha_baja',)
+    list_filter = ('categoria_socio','frecuencia_pago',)
+    search_fields = ('id_persona',)
+admin.site.register(Socio,SocioAdmin)
+
+admin.site.register(Categoria_socio)
