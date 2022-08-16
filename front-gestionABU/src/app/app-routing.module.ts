d1a673f194a3cfde8a76fb60958741cac3a0ed1f
danilopezyejas <49251328+danilopezyejas@users.noreply.github.com>
2022-08-16 19:54:42 -0300

    Agrege la vista de login

--- front-gestionABU/src/app/app-routing.module.ts
+++ front-gestionABU/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin', component: HeaderComponent },
+  { path: '**', redirectTo:'', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-gestionABU/src/app/app.module.ts
+++ front-gestionABU/src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MaterialModule } from './material.module';
+
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
