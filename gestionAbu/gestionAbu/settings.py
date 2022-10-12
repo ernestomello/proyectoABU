7ac45f9794ac31396c0fdc27695f8adc5ce25316
Ernesto Mello <emello969@gmail.com>
2022-10-12 12:07:13 -0300

    Update settings.py

--- gestionAbu/gestionAbu/settings.py
+++ gestionAbu/gestionAbu/settings.py
@@ -22,7 +22,7 @@ USE_THOUSAND_SEPARATOR = True
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&#n0$$+87s8^b1@y37*=!jfx=fj19-2gh+yia0fx&dq!4_4nl$'
+SECRET_KEY = 'django-insecure-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
